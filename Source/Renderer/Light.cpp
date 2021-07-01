d73689d0628bdefd99df5140be01dc18dcef23a2
Gábor Szalóki <gigu92@gmail.com>
2021-07-01 14:18:10 +0200

    !R shortened Light class, added GetDirection function

--- Source/Renderer/Light.cpp
+++ Source/Renderer/Light.cpp
@@ -7,52 +7,11 @@ Light::Light()
 	yaw = pitch = 0.0f;
 }
 
-Light::~Light()
+XMVECTOR Light::GetDirection() const
 {
-}
-
-float Light::GetIntensity()
-{
-	return intensity;
-}
-
-void Light::SetIntensity(float intensity)
-{
-	this->intensity = intensity;
-}
-
-XMFLOAT4 Light::GetColor()
-{
-	return color;
-}
-
-void Light::SetColor(XMFLOAT4 color)
-{
-	this->color = color;
-}
-
-float Light::GetYaw()
-{
-	return yaw;
-}
-
-void Light::SetYaw(float yaw)
-{
-	this->yaw = yaw;
-}
-
-float Light::GetPitch()
-{
-	return pitch;
-}
-
-void Light::SetPitch(float pitch)
-{
-	this->pitch = pitch;
-}
-
-void Light::SetRotation(float yaw, float pitch)
-{
-	this->yaw = yaw;
-	this->pitch = pitch;
+	float cosPitch = cosf(pitch);
+	float x = sinf(yaw) * cosPitch;
+	float y = -sinf(pitch);
+	float z = cosf(yaw) * cosPitch;
+	return XMVectorSet(x, y, z, 0);
 }
--- Source/Renderer/Light.h
+++ Source/Renderer/Light.h
@@ -7,17 +7,17 @@ class Light
 public:
 
 	Light();
-	~Light();
 
-	float GetIntensity();
-	void SetIntensity(float intensity);
-	XMFLOAT4 GetColor();
-	void SetColor(XMFLOAT4 color);
-	float GetYaw();
-	void SetYaw(float yaw);
-	float GetPitch();
-	void SetPitch(float pitch);
-	void SetRotation(float yaw, float pitch);
+	float GetIntensity() const { return intensity; }
+	void SetIntensity(float intensity_) { intensity = intensity_; }
+	XMFLOAT4 GetColor() const { return color; }
+	void SetColor(XMFLOAT4 color_) { color = color_; }
+	float GetYaw() const { return yaw; }
+	void SetYaw(float yaw_) { yaw = yaw_; }
+	float GetPitch() const { return pitch; }
+	void SetPitch(float pitch_) { pitch = pitch_; }
+	void SetRotation(float yaw_, float pitch_) { yaw = yaw_; pitch = pitch_; }
+	XMVECTOR GetDirection() const;
 
 private:
 
