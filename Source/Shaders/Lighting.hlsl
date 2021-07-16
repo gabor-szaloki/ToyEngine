232e0c3cd648026091dcc9ce3a4de00c6080e8c4
Gábor Szalóki <gigu92@gmail.com>
2021-07-16 15:52:07 +0200

    !F fixed cosTheta passed to FresnelSchlick

--- Source/Shaders/Lighting.hlsl
+++ Source/Shaders/Lighting.hlsl
@@ -8,14 +8,16 @@ float3 GGX_Specular(float3 normal, float3 lightVector, float3 viewVector, float 
 {
 	float roughness = perceptualRoughness * perceptualRoughness;
 	float3 halfVector = normalize(lightVector + viewVector);
-	float vDotN = max(dot(viewVector, normal), 0);
-	float hDotN = max(dot(halfVector, normal), 0);
 
 	// Distribution
 	float D = NormalDistributionGGXTR(normal, halfVector, roughness);
 
-	// Calculate fresnel
-	float3 F = FresnelSchlick(vDotN, F0);
+	// Calculate Fresnel
+	// Note: instead of nDotV, we pass hDotV as cosTheta to Fresnel, because halfway vector represents the microsurface normal that reflects
+	//       light in the view direction. If microsurface normal would face in any other directions, it wouldn't pass the NDF test. This is
+	//       explained in detail in the following comment: http://disq.us/p/1etzl77 under this article: https://learnopengl.com/PBR/Lighting
+	float cosTheta = saturate(dot(halfVector, viewVector));
+	float3 F = FresnelSchlick(cosTheta, F0);
 	kS = F;
 
 	// Geometry term
@@ -24,6 +26,8 @@ float3 GGX_Specular(float3 normal, float3 lightVector, float3 viewVector, float 
 
 	// Calculate the Cook-Torrance nominator and denominator
 	float3 nom = D * G * F;
+	float vDotN = max(dot(viewVector, normal), 0);
+	float hDotN = max(dot(halfVector, normal), 0);
 	float denom = saturate(4 * vDotN * hDotN + 0.05);
 
 	return nom / denom;
